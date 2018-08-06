261350f6b66ad6f448cec8ebf52c9d1a4d8f4906
ZhangZiChun <188484592@qq.com>
2018-08-07 07:17:40 +0800

    Add files via upload

--- AlignNet.py
+++ AlignNet.py
@@ -9,19 +9,22 @@ class AlignNet(torch.nn.Module):
 	def __init__(self):
 		super(AlignNet,self).__init__()
 		self.down1x = DownLayer(3,16)
-		self.down2x = DownLayer(16,32)
-		self.down3x = DownLayer(32,64)
+		self.down2x = DownLayer(16,64)
+		self.down3x = DownLayer(64,128)
+		self.down4x = DownLayer(128,256)
         
 		self.down1y = DownLayer(3,16)
-		self.down2y = DownLayer(16,32)
-		self.down3y = DownLayer(32,64)
+		self.down2y = DownLayer(16,64)
+		self.down3y = DownLayer(64,128)
+		self.down4y = DownLayer(128,256)
 
 		#kron 部分的源码还有待修改，其现在的版本包含了不止matching，masked的部分
-		self.kron = KronEmbed(64)
+		self.kron = KronEmbed()
 
-		self.up1 = UpLayer(64,32)
-		self.up2 = UpLayer(32,16)
-		self.up3 = UpLayer(16,3)
+		self.up1 = UpLayer(256,128)
+		self.up2 = UpLayer(128,64)
+		self.up3 = UpLayer(64,16)
+		self.up4 = UpLayer(16,3)
 
 	def forward(self,img_x,img_y):
 		out_x = self.down1x(img_x)
@@ -30,6 +33,8 @@ class AlignNet(torch.nn.Module):
 		out_y = self.down2y(out_y)
 		out_x = self.down3x(out_x)
 		out_y = self.down3y(out_y)
+		out_x = self.down4x(out_x)
+		out_y = self.down4y(out_y)
 
 		#这里注意清楚是masked谁，后期联合kron一并修改
 		masked_y = self.kron(out_x,out_y)
@@ -37,6 +42,7 @@ class AlignNet(torch.nn.Module):
 		out = self.up1(masked_y)
 		out = self.up2(out)
 		out = self.up3(out)
+		out = self.up4(out)
 
 		return out
 
--- train.py
+++ train.py
@@ -19,10 +19,11 @@ base_path = "H:/pedestrian_RGBT/kaist-rgbt/images/"
 log_interval = 1
 checkpoint_interval = 5
 learning_rate = 0.001
-checkpoint_model_dir = "H:/model_checkPoint_save_version2/"
+checkpoint_model_dir = "H:/model_checkPoint_save_version4/"
 save_model_dir = "H:/model_finish/"
 epochs = 10
-
+loss_weight = 1e5
+batch_size = 4
 
 
 #transform method
@@ -44,10 +45,22 @@ transform2 = transforms.Compose([
 alignNet = AlignNet().to(device)
 optimizer = Adam(alignNet.parameters(),learning_rate)
 mse_loss = torch.nn.MSELoss()
+
+imgR = list()
+imgT = list()
+imgT_trans = list()
+imgR_batch = torch.zeros(batch_size,3,512,512)
+imgT_batch = torch.zeros(batch_size,3,512,512)
+imgT_trans_batch = torch.zeros(batch_size,3,512,512)
+
+flag = 0
+global log_check
+global log_loss_sum
+
 #best_loss = infinite_great
 
 #temp Area~~~~~~~~
-alignNet.load_state_dict(torch.load('H:\\model_checkPoint_save\\ckpt_epoch_0_Sequence_id_5.plk'))
+#alignNet.load_state_dict(torch.load('H:\\model_checkPoint_save_version3\\ckpt_epoch_3_Sequence_id_40.plk'))
 
 
 
@@ -68,30 +81,69 @@ for e in range(epochs):
             imgs = os.listdir(rgb_folder)
             imgNum = len(imgs) 
             
+            
+            flag = 0
+            imgR = []
+            imgT = []
+            imgT_trans = []
+            
+            log_check = 0
+            log_loss_sum = 0
+            
             for imgNo in range(imgNum):
-                optimizer.zero_grad()
+                flag = flag + 1
+                
                 encodeNo = list(str(imgNo+100000))
                 encodeNo.pop(0)
                 encodeNo = "".join(encodeNo)
                 
                 img1 = Image.open(rgb_folder+"I"+encodeNo+".jpg")
                 img2 = Image.open(thermal_folder+"I"+encodeNo+".jpg")
-                img1 = transform2(img1).unsqueeze(0)
-                img2_trans = transform(img2).unsqueeze(0)
-                img2 = transform2(img2).unsqueeze(0).to(device)
+                img1 = transform2(img1)#.unsqueeze(0)
+                img2_trans = transform(img2)#.unsqueeze(0)
+                img2 = transform2(img2)#.unsqueeze(0).to(device)
+                
+                imgR.append(img1)
+                imgT_trans.append(img2_trans)
+                imgT.append(img2)
+                
+                if flag == batch_size:
+                    
+                    flag = 0
+                    
+                    for p in range (batch_size):
+                        imgR_batch[p] = imgR[p]
+                        imgT_batch[p] = imgT[p]
+                        imgT_trans_batch[p] = imgT_trans[p]
+                        
+                    optimizer.zero_grad()
+                    #img2_masked = alignNet(img1.to(device),img2_trans.to(device))
+                    imgT_batch = imgT_batch.to(device)
+                    imgR_batch = imgR_batch.to(device)
+                    imgT_trans_batch = imgT_trans_batch.to(device)
+                    
+                    img_masked = alignNet(imgR_batch , imgT_trans_batch)
 
-                img2_masked = alignNet(img1.to(device),img2_trans.to(device))
 
-                loss = mse_loss(img2_masked,img2)
-                loss.backward()
-                optimizer.step()
+                    loss = mse_loss(img_masked , imgT_batch)/batch_size
+                    loss = loss * loss_weight
+                    loss.backward()
+                    optimizer.step()
 
-                log_loss = loss.item()
+                    log_loss = loss.item()
+                    log_check = log_check + 1
+                    log_loss_sum = log_loss_sum + log_loss
+                    
+                    imgR = []
+                    imgT = []
+                    imgT_trans = []
+            
+            
             turn = turn + 1
-
+            log_loss_sum = log_loss_sum/log_check
             if turn % log_interval == 0:
                 mesg = "{}\tEpoch {}  Turn{}:  set {}  sequence {}  loss: {:.6f}".format(
-                      time.ctime(), e + 1, turn, i, j, log_loss)
+                      time.ctime(), e + 1, turn, i, j, log_loss_sum)
                 print(mesg)
 
 
